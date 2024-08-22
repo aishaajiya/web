eb10196026dd6280c663db4d868c5cb83aaf3922
aishaajiya <127016238+aishaajiya@users.noreply.github.com>
2024-08-22 08:48:06 +0100

    Update home.js

--- home.js
+++ home.js
@@ -72,50 +72,50 @@ const fourth = () => {
 const num1 = () => {
       document.getElementById('slide').src= 'Aisha5.png';
       }
-    setInterval(num1 , 10000);
+    setInterval(num1 , 11000);
 
 const num2 = () => {
       document.getElementById('slide').src= 'Aishagrad1.png';
       }
-    setInterval(num2 , 10000);
+    setInterval(num2 , 12000);
 
 const num3 = () => {
       document.getElementById('slide').src= 'Aishagrad2.png';
       }
-    setInterval(num3 , 10000);
+    setInterval(num3 , 13000);
   
 const num4 = () => {
       document.getElementById('slide').src= 'Aishagrad3.png';
       }
-    setInterval(num4 , 10000);
+    setInterval(num4 , 14000);
 
 const num5 = () => {
       document.getElementById('slide').src= 'Aishagrad4.png';
       }
-    setInterval(num5 , 10000);
+    setInterval(num5 , 15000);
 
 const num6 = () => {
       document.getElementById('slide').src= 'Aishagrad5.png';
       }
-    setInterval(num6 , 10000);
+    setInterval(num6 , 16000);
 
 const num7 = () => {
       document.getElementById('slide').src= 'Aishagrad6png';
       }
-    setInterval(num7 , 10000);
+    setInterval(num7 , 17000);
 
 const num8 = () => {
       document.getElementById('slide').src= 'Aishagrad7.png';
       }
-    setInterval(num8 , 10000);
+    setInterval(num8 , 18000);
 
 
 const num9 = () => {
       document.getElementById('slide').src= 'Aishagrad8.png';
       }
-    setInterval(num9 , 10000);
+    setInterval(num9 , 19000);
 
 const num10 = () => {
       document.getElementById('slide').src= 'Aishagrad9.png';
       }
-    setInterval(num10 , 10000);
+    setInterval(num10 , 20000);
